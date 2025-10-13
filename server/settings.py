70d5953536d985a5b5ffb3e34015096b2b97f483
Morison <morisonabinsinguza@gmail.com>
2025-10-13 14:51:58 +0300

    Add rest_framework, corsheaders to installed apps

--- server/settings.py
+++ server/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'serverapp',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
