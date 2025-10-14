072454fa75b3616809c573c3910661ba528e3ea4
Morison <morisonabinsinguza@gmail.com>
2025-10-14 18:36:26 +0300

    Add @heroicons/react dependency and update Vite server configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,4 +8,13 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    port: 3000,
+    proxy: {
+      'api/': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
