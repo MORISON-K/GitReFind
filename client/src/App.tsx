2610574932b3c030f6c6f889f03a0e5eaa936c65
Morison <morisonabinsinguza@gmail.com>
2025-10-15 08:35:42 +0300

    Update app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Search from "./pages/SearchPage"
-import Details from "./pages/DetailsPage"
-import Home from "./pages/HomePage"
-import History from "./pages/HistoryPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import RepoDetail from "./pages/RepoDetailPage";
+import HistoryPage from "./pages/HistoryPage";
+import Navbar from "./components/Navbar";
 
-export default function App(){
-  return(
-    <>
+const App: React.FC = () => {
+  return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/" element={<Search/>}/>
-        <Route path="/" element={<Details/>}/>
-        <Route path="/" element={<History/>}/>
-      </Routes>
+      <Navbar />
+      <main className="px-4 sm:px-6 lg:px-8 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/repo/:owner/:repo" element={<RepoDetail />} />
+        </Routes>
+      </main>
     </BrowserRouter>
-    </>
-  )
-}
+  );
+};
+
+export default App;
